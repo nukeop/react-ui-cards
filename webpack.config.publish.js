79a666d40804e6b2e9e03410b73b98909a0d25b5
nukeop <alsw@protonmail.com>
2018-01-16 14:37:46 +0100

    Prepublish webpack setup

---
+++ webpack.config.publish.js
@@ -0,0 +1,31 @@
+const webpack = require('webpack');
+const path = require('path');
+
+const SRC_DIR = path.resolve(__dirname, 'src');
+const DIST_DIR = path.resolve(__dirname, 'dist');
+
+module.exports = {
+  entry: path.resolve(SRC_DIR, 'index.js'),
+  output: {
+    path: DIST_DIR,
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?/,
+        loader: 'babel-loader'
+      }, {
+        test: /\.css/,
+        loader: 'style-loader!css-loader?modules=true&localIdentName=[name]__[local]___[hash:base64:5]'
+      }, {
+        test: /\.scss$/,
+        loader: 'style-loader!css-loader?importLoaders=1&modules&localIdentName=[local]!sass-loader'
+      }
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin(),
+    new webpack.NamedModulesPlugin()
+  ]
+};
