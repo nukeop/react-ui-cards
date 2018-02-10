42f673dd1e80d4e4fcb5b3c2119cdb506642ac89
nekonee <haniasa96@gmail.com>
2018-02-11 00:11:33 +0100

    Started news header card

---
+++ src/NewsHeaderCard/index.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+import Card from '../Card';
+import NewsHeader from './NewsHeader';
+import NewsHeaderBackground from './NewsHeaderBackground';
+
+import './styles.scss';
+
+class NewsHeaderCard extends React.Component{
+  constructor(props){
+    super(props);
+  }
+
+  render(){
+    let{
+      href,
+      title,
+      author,
+      date,
+      thumbnail
+    } = this.props;
+    return(
+      <a href={href}>
+	<Card className='news-header-card'>
+	  
+	   <NewsHeaderBackground
+	      thumbnail={thumbnail}
+	      />
+	   
+	   <NewsHeader
+	      title={title}
+	      author={author}
+	      date={date}
+	      />
+	 </Card>
+      </a> 
+    );
+  }
+}
+
+export default NewsHeaderCard;
