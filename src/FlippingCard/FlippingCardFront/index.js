3beb2921c830feebcfbf743cb450038791ccc4f5
nukeop <alsw@protonmail.com>
2018-05-23 20:58:50 +0200

    Update flipping cards

--- src/FlippingCard/FlippingCardFront/index.js
+++ src/FlippingCard/FlippingCardFront/index.js
@@ -1,19 +1,21 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './styles.scss';
 
-class FlippingCardFront extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
-  render() {
-    return (
-      <div className='front'>
-	{this.props.children}
-      </div>
-    );
-  }
+const FlippingCardFront = props => {
+  return (
+    <div className='front'>
+      {props.children}
+    </div>
+  );
 }
+
+FlippingCardFront.propTypes = {
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.element),
+    PropTypes.element
+  ])
+};
 
 export default FlippingCardFront;
