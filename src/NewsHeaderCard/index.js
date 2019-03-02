3654d97f5ce6686c37bd1e0fa48eabf197de668f
Dmitry Kolesnikov <dmkolesnikov@gmail.com>
2019-03-02 12:43:29 +0200

    add dist for local testing

--- src/NewsHeaderCard/index.js
+++ src/NewsHeaderCard/index.js
@@ -22,8 +22,8 @@ class NewsHeaderCard extends React.Component{
       tags
     } = this.props;
     return(
-      <a href={href}>
-	<Card className='news-header-card' onClick={onClick}>
+      <a href={href} onClick={onClick}>
+	<Card className='news-header-card'>
 	  
 	   <NewsHeaderBackground
 	      thumbnail={thumbnail}
