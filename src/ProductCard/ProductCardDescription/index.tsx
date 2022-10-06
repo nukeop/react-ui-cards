4a8151c662d7fbc954edef4e7a98d0b1a28232c8
RiteshKSingh1709 <ritesh.kumar465@gmail.com>
2022-10-06 22:17:55 +0530

    added support for disabled button in product card

--- src/ProductCard/ProductCardDescription/index.tsx
+++ src/ProductCard/ProductCardDescription/index.tsx
@@ -7,20 +7,22 @@ export type ProductCardDescriptionProps = {
   description: string;
   buttonText?: string;
   url: string;
+  isButtonActive?: boolean;
 };
 
 const ProductCardDescription: React.FC<ProductCardDescriptionProps> = ({
   productName,
   description,
   buttonText,
-  url
+  url,
+  isButtonActive = true
 }) => (
   <div className={styles['product-card-description-box']}>
     <div className={styles['product-card-name']}>{productName}</div>
     <p className={styles['product-card-description']}>{description}</p>
     {buttonText &&
       <div className={styles['row']}>
-        <a className={styles['buy-button']} href={url}>
+        <a className={isButtonActive ? styles['buy-button'] : styles['buy-button-disabled']} href={isButtonActive ? url : "#"}>
           {buttonText}
         </a>
       </div>}
