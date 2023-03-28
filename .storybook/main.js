3117b61cffc3dd99b00c25ffa89a8510bfb6054c
aleksandar-polic <aleksandar.polic@marbleit.rs>
2023-03-28 11:49:37 +0200

    Update Sass package. Update Storybook to version 7, to be compliant with Node 18 LTS version.

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,21 +1,23 @@
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    //"@storybook/preset-create-react-app",
-    '@storybook/preset-scss'
-  ],
-  webpackFinal: async (config) => {
-    config.module.rules.find(({ test }) => test.test('.svg')).test = /\.(ico|jpg|jpeg|png|apng|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/;
-
-    config.module.rules.unshift({  
-      test: /\.svg$/,  
+  "stories": ["../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials",
+  //"@storybook/preset-create-react-app",
+  '@storybook/preset-scss', "@storybook/addon-mdx-gfm"],
+  webpackFinal: async config => {
+    config.module.rules.find(({
+      test
+    }) => test.test('.svg')).test = /\.(ico|jpg|jpeg|png|apng|gif|eot|otf|webp|ttf|woff|woff2|cur|ani|pdf)(\?.*)?$/;
+    config.module.rules.unshift({
+      test: /\.svg$/,
       use: "raw-loader"
     });
-
     return config;
+  },
+  framework: {
+    name: "@storybook/react-webpack5",
+    options: {}
+  },
+  docs: {
+    autodocs: true
   }
-}
+};
